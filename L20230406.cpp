a1ee875fb540bae4e1fb8b99a69c99cb3e375136
yournotprogrammer <xkr403@gmail.com>
2023-04-06 09:38:39 +0900

    create cpp

--- L20230406.cpp
+++ L20230406.cpp
@@ -1,52 +1,11 @@
 #include <iostream>
+#include "Player.h"
+#include "Monster.h"
 
 using namespace std;
 
-class Player 
+int main() 
 {
-public:
-	void Attatck()
-	{
-	}
-	void MoveRight() 
-	{
-	}
-	void MoveLeft()
-	{
-	}
-	void MoveUp()
-	{
-	}
-	void MoveDown()
-	{
-	}
-private:
-	int HP = 100;
-	int MP = 100;
-};
-
-class Monster {
-public:
-	void Attatck() 
-	{
-	}
-	void MoveRight()
-	{
-	}
-	void MoveLeft()
-	{
-	}
-	void MoveUp()
-	{
-	}
-	void MoveDown()
-	{
-	}
-private:
-	int HP = 100;
-};
-
-int main() {
 	Player Hero;
 	Monster Goblin;
 	Monster WildBoar;
