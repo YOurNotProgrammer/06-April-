4e8245ad1d684f7a9e92c0f33680d7be07efe00e
yournotprogrammer <xkr403@gmail.com>
2023-04-06 09:45:33 +0900

    Create h, cpp

---
+++ Monster.h
@@ -0,0 +1,23 @@
+#pragma once
+class Monster
+{
+public:
+	void Attatck()
+	{
+	}
+	void MoveRight()
+	{
+	}
+	void MoveLeft()
+	{
+	}
+	void MoveUp()
+	{
+	}
+	void MoveDown()
+	{
+	}
+private:
+	int HP = 100;
+};
+
---
+++ Player.h
@@ -0,0 +1,24 @@
+#pragma once
+class Player
+{
+public:
+	void Attatck()
+	{
+	}
+	void MoveRight()
+	{
+	}
+	void MoveLeft()
+	{
+	}
+	void MoveUp()
+	{
+	}
+	void MoveDown()
+	{
+	}
+private:
+	int HP = 100;
+	int MP = 100;
+};
+
